335669f01b69d787cf5971b6e038004fddc8950d
limoncc <limoncc@icloud.com>
2026-08-27 21:33:41 +0800

    fix(sdk): 版本元数据对齐 0.1.1，PyO3 版本断言改为与包版本动态比较

--- trailer-sdk/trailer/__init__.py
+++ trailer-sdk/trailer/__init__.py
@@ -1,6 +1,6 @@
 """Trailer: Next-gen ML experiment tracking."""
 
-__version__ = "0.1.0"
+__version__ = "0.1.1"
 
 # The compiled Rust extension (cdylib) is importable as trailer.trailer
 # after `maturin develop` or `pip install`
