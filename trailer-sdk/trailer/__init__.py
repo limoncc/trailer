1d0cf832513e5a5ad5b5fffb5a78eeb73a82a40b
limoncc <limoncc@icloud.com>
2026-08-31 17:04:50 +0800

    chore(release): bump version to 0.1.4

--- trailer-sdk/trailer/__init__.py
+++ trailer-sdk/trailer/__init__.py
@@ -1,6 +1,6 @@
 """Trailer: Next-gen ML experiment tracking."""
 
-__version__ = "0.1.3"
+__version__ = "0.1.4"
 
 # The compiled Rust extension (cdylib) is importable as trailer.trailer
 # after `maturin develop` or `pip install`
